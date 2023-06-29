95e4e2a4d7acbb0f79a4a118d7983e7ab54cf485
Foxy224 <ppaaa4@gmail.com>
2023-06-29 17:08:25 +0300

    //

--- src/main/java/ru/kata/spring/boot_security/demo/service/UsersServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UsersServiceImpl.java
@@ -1,26 +1,30 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repositories.UsersRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UsersServiceImpl implements UsersService {
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersServiceImpl(UsersRepository usersRepository) {
+    public UsersServiceImpl(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void add(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
     }
 
@@ -35,7 +39,15 @@ public class UsersServiceImpl implements UsersService {
     }
 
     @Transactional
-    public void update(User user) {
+    public void update(User updatedUser) {
+        User user = usersRepository.findByUsername(updatedUser.getUsername());
+        if (!updatedUser.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+        user.setName(updatedUser.getName());
+        user.setYears(updatedUser.getYears());
+        user.setUsername(updatedUser.getUsername());
+        user.setRoles((Set<Role>) updatedUser.getRoles());
         usersRepository.save(user);
     }
     @Transactional
